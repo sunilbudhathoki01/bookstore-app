9bb726a8cff633b40b95b69f4a69dea1aadd1ba5
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-11-11 15:40:37 +0545

    git init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+   <>
+    <div className="text-3xl font-bold underline">
+    </div>
+    <Navbar></Navbar></>
+  )
+}
+
+export default App
