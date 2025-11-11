9dea078913f644386e80e5ba3c688bab5e075d32
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-11-11 19:09:55 +0545

    static landing page design complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
    <>
-    <div className="text-3xl font-bold underline">
-    </div>
-    <Navbar></Navbar></>
+   <Navbar></Navbar>
+   <Banner></Banner>
+   <Footer></Footer>
+  </>
   )
 }
 
